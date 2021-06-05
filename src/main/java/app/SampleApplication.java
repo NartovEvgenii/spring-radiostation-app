2092b61a93318efbbd1cda0327c3d2b4bfc0be65
NartovEvgenii <evgeniynartov36rus@gmail.com>
2021-06-05 09:41:52 +0300

    ref

--- src/main/java/app/SampleApplication.java
+++ src/main/java/app/SampleApplication.java
@@ -1,20 +1,11 @@
 package app;
 
-import app.config.ApplicationConfig;
-import app.repository.OrderRepository;
-import app.service.OrderService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@Configuration
 @EnableAutoConfiguration
-@ComponentScan
-@EnableJpaRepositories(basePackages = {"app.repository"})
 public class SampleApplication {
 
     public static void main(String[] args) {
